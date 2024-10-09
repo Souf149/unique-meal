dd693068e4fbae1af55a0f09504cf0ac9765c54a
Ciciliagithub <79082351+Ciciliagithub@users.noreply.github.com>
2024-10-09 14:41:54 +0200

    createaccoun&validator bijgevoegd
    create account verbeterd
    validators toegevoegd

--- src/functionalities/create_account.py
+++ src/functionalities/create_account.py
@@ -33,11 +33,11 @@ def choose_city():
 
     while True:
         try:
-            choice = int(input("Enter a number from 1-10: "))
+            choice = int(input("Enter a number from [1]-[10]: "))
             if 1 <= choice <= 10:
                 return city_list[choice - 1]
             else:
-                print("Invalid choice. Please choose a number between 1 and 10.")
+                print("Invalid choice. Please choose a number between [1] and [10].")
         except ValueError:
             print("Invalid input. Please enter a number.")
 
@@ -45,46 +45,69 @@ def choose_city():
 def create_new_user(db: Connection, user: dict):
     Validator = User_Info_Validator
     while True:
+        message = ""
         while True:
             clear_terminal_with_title()
+            if (message):
+                print(message)
             f_name = user_input("Input first name please")
             if Validator.validate_name(f_name):
+                message = ""
                 break
             else:
-                print("Invalid option! Example: Mark")
+                message = "Invalid option! Example: Mark"
 
         while True:
+            clear_terminal_with_title()
+            if (message):
+                print(message)
             l_name = user_input("Input last name please")
             if Validator.validate_name(l_name):
+                message = ""
                 break
+            else:
+                message = "Invalid option! Example: zuckerberg"
 
         while True:
-            password = user_input(
-                "Input the password.\nmust be unique and have a length of at least 8 characters\nmust be no longer than 10 characters\nmust be started with a letter or underscores (_)\ncan contain letters (a-z), numbers (0-9), underscores (_), apostrophes ('), and periods (.)\nno distinguish between lowercase or uppercase letters"
-            )
+            clear_terminal_with_title()
+            if (message):
+                print(message)
+            password = user_input("Input the password.\nmust be unique and have a length of at least 8 characters\nmust be no longer than 10 characters\nmust be started with a letter or underscores (_)\ncan contain letters (a-z), numbers (0-9), underscores (_), apostrophes ('), and periods (.)\nno distinguish between lowercase or uppercase letters")
             if Validator.validate_password(password):
+                message = ""
                 break
+            else:
+                message = "Incorrect password! Follows the instructions on how to construct a password"
 
         hashed_pass = hash_password(password)
 
         while True:
+            clear_terminal_with_title()
+            if (message):
+                print(message)
             level = user_input(
-                f"What level should this user have access to?. Important: You can only make users of level: {Level.NAMES[user['level']]}"
+                f"What level should this user have access to?. Important: You can only make users of level: {Level.NAMES[user['level']]} and lower"
             )
             if Validator.validate_level(level) and level <= user["level"]:
+                message = ""
                 break
+            else:
+                message = "Incorrect level! Following the instructions of the user levels you can make"
 
         while True:
+            clear_terminal_with_title()
+            if (message):
+                print(message)
             print(
-                "For the final info, please choose a name for the user.\nmust be unique and have a length of at least 8 characters\nmust be no longer than 10 characters\nmust be started with a letter or underscores (_)\ncan contain letters (a-z), numbers (0-9), underscores (_), apostrophes ('), and periods (.)\nno distinguish between lowercase or uppercase letters"
+                "For the final info, please choose an username for the user.\nmust be unique and have a length of at least 8 characters\nmust be no longer than 10 characters\nmust be started with a letter or underscores (_)\ncan contain letters (a-z), numbers (0-9), underscores (_), apostrophes ('), and periods (.)\nno distinguish between lowercase or uppercase letters"
             )
             username = user_input("Input the username")
             if Validator.validate_username(username):
+                message = ""
                 break
-        while True:
-            membership_id = Validator.validate_id(generate_id())  # membershipID
-            if membership_id:
-                break
+            else:
+                message = "Incorrect username! Following the requirements of the username you can make"
+        
         db.addUser(
             create_user_tuple(
                 generate_id(),
@@ -107,95 +130,154 @@ def create_new_user(db: Connection, user: dict):
 def create_new_member(db: Connection, user: dict):
     Validator = User_Info_Validator
     while True:
+        message = ""
         while True:
             clear_terminal_with_title()
-            f_name = user_input("Input first name please")
+            if (message):
+                print(message)
+            f_name = user_input("Input first name please").lower()
             if Validator.validate_name(f_name):
+                message = ""
                 break
             else:
-                print("Invalid option! Example: Mark")
+                message = "Invalid option! Example: Mark"
 
         while True:
+            clear_terminal_with_title()
+            if (message):
+                print(message)
             l_name = user_input("Input last name please")
             if Validator.validate_name(l_name):
+                message = ""
                 break
+            else:
+                message =  "Invalid option! Example: zuckerberg"
 
         while True:
+            clear_terminal_with_title()
+            if(message):
+                print(message)
             age = user_input("Input a valid age please")
             if Validator.validate_age(age):
+                message = ""
                 break
             else:
-                print("Invalid option. Your age has to be between 0 and 120")
+                message = "Invalid option. Your age has to be between 0 and 120"
 
         while True:
+            clear_terminal_with_title()
+            if (message):
+                print(message)
             gender = user_input("Input 'f' or 'm' to choose the gender")
             if Validator.validate_gender(gender):
+                message = ""
                 break
             else:
-                print("Invalid option!")
+                message = "Invalid option!"
 
         while True:
+            clear_terminal_with_title()
+            if (message):
+                print(message)
             weight = user_input("Input the weight")
             if Validator.validate_weight(weight):
-                break
-
-        while True:
-            street = user_input("Input the street name")
-            if Validator.validate_name(street):
+                message = ""
                 break
             else:
-                print("Incorrect street name!")
+                message = "Incorrect weight number! Weight has to be a number between 0 and 300kg! Example: 75kg , 75.6kg"
 
         while True:
+            clear_terminal_with_title()
+            if (message):
+                print(message)
+            street = user_input("Input the street name")
+            if Validator.validate_street_name(street):
+                message = ""
+                break
+            else:
+                message = "Incorrect street name!"
+
+        while True:
+            clear_terminal_with_title()
+            if (message):
+                print(message)
             house_number = user_input("Input the house number")
             if Validator.validate_housenumber(house_number):
                 break
+            else:
+                message = "Incorrect house number! house number has to be any number for 0 - 9999 "
 
         while True:
+            clear_terminal_with_title()
+            if (message):
+                print(message)
             zip = user_input(
                 "Input the zipcode. \n Example: Zip Code (DDDDXX) - only enter DDDDXX "
             )
             if Validator.validate_zip(zip):
+                message = ""
                 break
+            else:
+                message = "Incorrect zip number! Example: 5353EK"
 
         while True:
+            clear_terminal_with_title()
             city = choose_city()
             if Validator.validate_name(city):
                 print(f"Chosen city{city}")
                 break
 
         while True:
+            clear_terminal_with_title()
+            if (message):
+                print(message)
             email = user_input("Input the emailaddress.")
             if Validator.validate_email(email):
+                message = ""
                 break
+            else:
+                message = "Incorrect e-mailaddress! Example: markzuckerburg@gmail.com | markzuckerburg_@hotmail.com | markzuckerburg@yahoo.com"
 
         while True:
+            clear_terminal_with_title()
+            if (message):
+                print(message)
             phone = user_input(
                 "Input the phone number.\nExample: (+31-6-DDDDDDDD) - only enter DDDDDDDD '"
             )
             if Validator.validate_phone(phone):
+                message = ""
                 break
+            else:
+                message = "Incorrect phone number! Only type the 8 digits after the +316 "
 
         while True:
+            clear_terminal_with_title()
+            if (message):
+                print(message)
             password = user_input(
                 "Input the password.\nmust be unique and have a length of at least 8 characters\nmust be no longer than 10 characters\nmust be started with a letter or underscores (_)\ncan contain letters (a-z), numbers (0-9), underscores (_), apostrophes ('), and periods (.)\nno distinguish between lowercase or uppercase letters"
             )
             if Validator.validate_password(password):
+                message = ""
                 break
+            else:
+                message = "Incorrect password! Follows the instructions on how to construct a password"
 
-        hashed_pass = hash_password(password)  # komt nog
+        hashed_pass = hash_password(password)
 
         while True:
+            clear_terminal_with_title()
             print(
                 "For the final info, please choose a name for the user.\nmust be unique and have a length of at least 8 characters\nmust be no longer than 10 characters\nmust be started with a letter or underscores (_)\ncan contain letters (a-z), numbers (0-9), underscores (_), apostrophes ('), and periods (.)\nno distinguish between lowercase or uppercase letters"
             )
             username = user_input("Input the username")
             if Validator.validate_username(username):
+                message = ""
                 break
-        while True:
-            membership_id = Validator.validate_id(generate_id())
-            if membership_id:
-                break
+            else:
+                message = ""
+
         db.addMember(
             create_member_tuple(
                 generate_id(),
--- src/imports/validator.py
+++ src/imports/validator.py
@@ -6,7 +6,7 @@ import re
 class User_Info_Validator:
 
     @staticmethod
-    def validate_id(membershipid: str) -> bool:
+    def validate_id(membershipid: str) -> bool: 
         digits = [int(digit) for digit in membershipid[:-1]]
         digit_sum = sum(digits)
         expected_checksum = digit_sum % 10
@@ -15,115 +15,102 @@ class User_Info_Validator:
         return actual_checksum == expected_checksum
 
     @staticmethod
-    def validate_password(password: str) -> bool:
-        if "\x00" in password:  # null-bytes check
-            return False
-
-        # Check for maximum length
-        if len(password) > 30:
-            return False
-
-        # regex pattern with specifically whitelisting
-        pattern = re.compile(
-            r"^(?=.*[a-z])"  # At least one lowercase letter
-            r"(?=.*[A-Z])"  # At least one uppercase letter
-            r"(?=.*[0-9])"  # At least one digit
-            r"(?=.*[~!@#$%&_\-+=`|\(){}[\]:;\'<>,.?/])"  # At least one special character
-            r".{12,30}$"  # Length between 12 and 30 characters
-        )
-
-        # Match the password against the compiled pattern
-        return bool(pattern.match(password))
-
-    @staticmethod
-    def validate_level(level: str | int) -> bool:
-        # Buffer overflow check: limit input length
-        max_length = 3  # Assuming level should be a maximum of 3 digits (to accommodate levels up to 999)
-        if isinstance(level, str) and len(level) > max_length:
-            print(f"Input level exceeds maximum length of {max_length} characters.")
-            return False
-
-        # Null-byte check
-        if "\0" in str(level):
-            print("Input level contains a null byte.")
-            return False
-
-        return True
-
-    @staticmethod
-    def validate_name(name: str) -> bool:
-        if not isinstance(name, str):
-            return False
-
-        name = name.strip()
-
-        if not (2 <= len(name) <= 50):
-            return False
-
-        if "\x00" in name:
-            return False
-
-        if not re.match("^[a-z]*$", name):  # First letter uppercase, rest lowercase
-            return False
-
-        return True
-
-    @staticmethod
-    def validate_street_name(street_name: str) -> bool:
-        if not isinstance(street_name, str):
-            return False
-
-        MAX_INPUT_LENGTH = 1024
-        if len(street_name) > MAX_INPUT_LENGTH:
-            return False
-
-        street_name = street_name.strip()
-
-        if not (2 <= len(street_name) <= 100):
-            return False
-
-        if "\x00" in street_name:
-            return False
-
-        if not re.match(r"^[A-Za-z .'-]+$", street_name):
-            return False
-
-        return True
-
-    @staticmethod
-    def validate_age(age) -> bool:
-        valid_ages = set(range(0, 121))
-
-        if age is None:
-            return False
-
-        if isinstance(age, str):
-            if "\0" in age:  # Check for null bytes
-                return False  # Reject input with null bytes
-            if len(age) > 3:
+    def validate_password(password: str) -> bool: #Gecheckt op whitelisten
+        if not "\x00" in password:
+            if len(password) > 30:
                 return False
-            if age.isdigit():  # Check if it's a numeric string
-                age = int(age)
-            else:
-                return False  # Reject non-numeric strings
 
-        return age in valid_ages
+            pattern = re.compile(
+                r"^(?=.*[a-z])"  
+                r"(?=.*[A-Z])"  
+                r"(?=.*[0-9])"  
+                r"(?=.*[~!@#$%&_\-+=`|\(){}[\]:;\'<>,.?/])" 
+                r".{12,30}$"  
+            )
+
+            return bool(pattern.match(password))
+
+        return False 
 
     @staticmethod
-    def validate_gender(gender: str) -> bool:
-        # Check for null bytes
-        if "\x00" in gender:
-            return False
+    def validate_level(level: str | int) -> bool: #Gecheckt op whitelisten
+        max_length = 3
 
-        MAX_LENGTH = 1
-        if len(gender) > MAX_LENGTH:
-            return False
+        if not "\0" in str(level):
+            if isinstance(level, str) and len(level) > max_length:
+                print(f"Input level exceeds maximum length of {max_length} characters.")
+                return False
 
-        # Validate against whitelisted values
-        if gender.upper() in {"M", "F"}:
+            return True
+
+        print("Input level contains a null byte.")
+        return False
+
+    @staticmethod
+    def validate_name(name: str) -> bool: #Gecheckt op whitelisten
+        if not "\x00" in name:
+            if not isinstance(name, str):
+                return False
+
+            if not (2 <= len(name) <= 50):
+                return False
+
+            if not re.match("^[a-z]+$", name): 
+                return False
+
             return True
 
         return False
+    
+    @staticmethod
+    def validate_street_name(street_name: str) -> bool: #Gecheckt op whitelisten
+        if not "\x00" in street_name:
+            if not isinstance(street_name, str):
+                return False
+
+            MAX_INPUT_LENGTH = 1024
+            if len(street_name) > MAX_INPUT_LENGTH:
+                return False
+
+            street_name = street_name.strip()
+
+            if not (2 <= len(street_name) <= 100):
+                return False
+
+            if not re.match(r"^[A-Za-z .'-]+$", street_name):
+                return False
+
+            return True 
+        return False 
+
+    @staticmethod
+    def validate_age(age) -> bool: #Gecheckt op whitelisten
+        valid_ages = set(range(0, 121))
+        agebool = False
+        if age is None:
+            return False
+        if "\0" not in age:
+            if isinstance(age, str):
+                if len(age) > 3:
+                    return False
+                if age.isdigit():  
+                    age = int(age)
+                else:
+                    return False  
+            agebool = age in valid_ages
+        return agebool
+    @staticmethod
+
+    def validate_gender(gender: str) -> bool: #Gecheckt op whitelisten
+        if not "\x00" in gender:
+            MAX_LENGTH = 1
+            if len(gender) > MAX_LENGTH:
+                return False
+
+            if gender.upper() in {"M", "F"}:
+                return True
+
+        return False  
 
     @staticmethod
     def validate_weight(weight) -> bool:
@@ -148,21 +135,16 @@ class User_Info_Validator:
         return False
 
     @staticmethod
-    def validate_zip(field: str) -> bool:
-        # Check for null bytes
-        if "\x00" in field:
-            return False
-
-        # Check for excessively long input (beyond expected size)
-        if len(field) > 100:  # Set an arbitrary limit, e.g., 100 characters
-            return False
-
-        if (
-            isinstance(field, str)
-            and len(field) == 6
-            and re.match(r"^\d{4}[A-Za-z]{2}$", field)
-        ):
-            return True
+    def validate_zip(field: str) -> bool: # Volledig gewhitelist
+        if not "\x00" in field:
+            if len(field) > 100: 
+                return False
+            if (
+                isinstance(field, str)
+                and len(field) == 6
+                and re.match(r"^\d{4}[A-Za-z]{2}$", field)
+            ):
+                return True
 
         return False
 
@@ -183,29 +165,29 @@ class User_Info_Validator:
         return bool(re.match(email_pattern, email))
 
     @staticmethod
-    def validate_phone(phone: str) -> bool:
+    def validate_phone(phone: str) -> bool: #volledig gewhitelist
         max_length = 8
 
-        # Check for null bytes in the input
-        if "\0" in phone:
-            return False
+        if not ("\0" in phone):
+            if len(phone) > max_length:
+                return False  
 
-        if len(phone) > max_length:
-            return False
+            phone = phone.strip()  
 
-        phone = phone.strip()
+            valid_length = 8
+            valid_characters = set("0123456789")
 
-        valid_length = 8
-        valid_characters = set("0123456789")
+            if len(phone) != valid_length:
+                return False 
 
-        if len(phone) != valid_length:
-            return False
 
-        for char in phone:
-            if char not in valid_characters:
-                return False
+            for char in phone:
+                if char not in valid_characters:
+                    return False  
 
-        return True
+            return True 
+
+        return False 
 
     @staticmethod
     def validate_registration_date(registration_date) -> bool:
@@ -234,30 +216,23 @@ class User_Info_Validator:
         return False
 
     @staticmethod
-    def validate_username(username: str) -> bool:
-        # Check for null bytes
-        if "\0" in username:
-            return False
+    def validate_username(username: str) -> bool:#Gewcheckt op whitelist
+        if not "\0" in username:
+            username = username.strip().lower()  
 
-        username = (
-            username.strip().lower()
-        )  # Normalize the username to lowercase and strip whitespace
+            MAX_LENGTH = 10
 
-        # Maximum length for usernames to prevent buffer overflows
-        MAX_LENGTH = 10
+            # Allow only valid length (8 to MAX_LENGTH characters)
+            if not (8 <= len(username) <= MAX_LENGTH):
+                return False
 
-        # Allow only valid length (8 to MAX_LENGTH characters)
-        if not (8 <= len(username) <= MAX_LENGTH):
-            return False
+            # 1. Must start with a letter (a-z) or an underscore (_)
+            if username[0].isalpha() or username[0] == "_":
+                # 2. Must contain only allowed characters: letters (a-z), numbers (0-9), underscores (_), apostrophes ('), and periods (.)
+                if re.match("^[a-z0-9_.']+$", username):
+                    return True
 
-        # Whitelist criteria:
-        # 1. Must start with a letter (a-z) or an underscore (_)
-        if username[0].isalpha() or username[0] == "_":
-            # 2. Must contain only allowed characters: letters (a-z), numbers (0-9), underscores (_), apostrophes ('), and periods (.)
-            if re.match("^[a-z0-9_.']+$", username):
-                return True
-
-        return False
+        return False  
 
     @staticmethod
     def validate_hashed_pass(hashed_pass: str) -> str:
@@ -266,39 +241,16 @@ class User_Info_Validator:
         raise ValueError("Invalid password")
 
     @staticmethod
-    def validate_housenumber(housenumber, max_length=10) -> bool:
+    def validate_housenumber(housenumber, max_length=10) -> bool: #Gecheckt op whitelist
         if isinstance(housenumber, int):
             return 0 <= housenumber <= 9999
+        
+        if "\0" not in housenumber:
+            if isinstance(housenumber, str):
+                if len(housenumber) > max_length:
+                    return False
 
-        if isinstance(housenumber, str):
-            # Whitelist: Must not contain null bytes
-            if "\0" in housenumber:
-                return False
-
-            if len(housenumber) > max_length:
-                return False
-
-            if re.match("^[0-9]+$", housenumber):
-                return 0 <= int(housenumber) <= 9999
-
+                if re.match("^[0-9]+$", housenumber):
+                    return 0 <= int(housenumber) <= 9999
+            return False
         return False
-
-    def to_dict(self) -> dict:
-        return {
-            "id": self.id,
-            "level": self.level,
-            "f_name": self.f_name,
-            "l_name": self.l_name,
-            "age": self.age,
-            "gender": self.gender,
-            "weight": self.weight,
-            "street": self.street,
-            "house_number": self.house_number,
-            "zip": self.zip,
-            "city": self.city,
-            "email": self.email,
-            "phone": self.phone,
-            "registration_date": self.registration_date,
-            "username": self.username,
-            "hashed_pass": self.hashed_pass,
-        }
